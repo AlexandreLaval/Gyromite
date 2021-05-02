84b035ceab5ca1818b3bd59239f1c31fa24d135c
Alexandre LAVAL <herstalex@gmail.com>
2021-05-02 10:57:47 +0200

    init

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
